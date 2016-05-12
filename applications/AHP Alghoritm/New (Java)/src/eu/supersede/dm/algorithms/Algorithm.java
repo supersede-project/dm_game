253b5058bec4ed21bf6a19e9a4666b6478f52f98
Andrea Sosi <sosi.andrea@gmail.com>
2016-05-12 11:01:07 +0200

    - cleaning logs from AHP alghoritm - reorder the folders with different AHp alghoritms - added commons math3

---
+++ applications/AHP Alghoritm/New (Java)/src/eu/supersede/dm/algorithms/Algorithm.java
@@ -0,0 +1,11 @@
+package eu.supersede.dm.algorithms;
+
+public abstract class Algorithm {
+	//attributes
+	
+	//constructors 
+	
+	//methods
+	public abstract Object execute();
+	//public abstract void execute( Object ... objects );
+}
--- applications/ahp-analyser/src/eu/supersede/algos/Resources.java
+++
@@ -1,8 +0,0 @@
-package eu.supersede.algos;
-
-/*
- * Files in the package eu.riscoss.server.res are automatically included in the .war
- */
-public class Resources {
-	
-}
