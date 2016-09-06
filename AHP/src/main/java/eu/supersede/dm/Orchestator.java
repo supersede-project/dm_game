40c2cfc15e923fc81626c8e645320342c8e5e9bc
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2016-09-06 15:15:40 +0200

    moved AHP and pushed to artifactory

---
+++ AHP/src/main/java/eu/supersede/dm/Orchestator.java
@@ -0,0 +1,11 @@
+package eu.supersede.dm;
+
+public class Orchestator {
+	//attributes
+	
+	//constructors
+	
+	//methods 
+    public void selectAlgorithm() {
+	}
+}
---
+++ AHP/src/main/java/eu/supersede/dm/methods/Method.java
@@ -0,0 +1,10 @@
+package eu.supersede.dm.methods;
+
+public abstract class Method {
+	//attributes
+	
+	//constructors 
+	
+	//methods
+	public abstract void execute();
+}
