121273709bedd5f8913bce9d654ea28571852e5c
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2016-09-07 16:00:04 +0200

    updating pages to jqxWidgets

--- applications/game-requirements/src/main/resources/static/js/games.js
+++ applications/game-requirements/src/main/resources/static/js/games.js
@@ -16,24 +16,14 @@ var app = angular.module('w5app');
 
 app.controllerProvider.register('games', function($scope, $http, $location) {
 	
-	$scope.Math = window.Math;
+	
 	
 	$scope.open_games = [];
 	$scope.closed_games = [];
 
-	$scope.pagination = { 'open' : {}, 'closed' : {}};
-	$scope.pagination.open.totalPages = 1;
-	$scope.pagination.open.length = 0;
-    $scope.pagination.open.itemsPerPage = 5;
-    $scope.pagination.open.currentPage = 0;
-
-	$scope.pagination.closed.totalPages = 1;
-	$scope.pagination.closed.length = 0;
-    $scope.pagination.closed.itemsPerPage = 5;
-    $scope.pagination.closed.currentPage = 0;
-	
-	$http.get('game-requirements/game')
-	.success(function(data) {
+	$http.get('game-requirements/game').
+	success(function(data) 
+	{
 		for(var i = 0; i < data.length; i++)
 		{
 			if(data[i].finished)
@@ -45,60 +35,108 @@ app.controllerProvider.register('games', function($scope, $http, $location) {
 				$scope.open_games.push(data[i]);
 			}
 		}
-
-		$scope.pagination.open.length = $scope.open_games.length;
-		$scope.pagination.open.totalPages = Math.max(1, Math.ceil($scope.pagination.open.length / $scope.pagination.open.itemsPerPage));
 		
-		$scope.pagination.closed.length = $scope.closed_games.length;
-		$scope.pagination.closed.totalPages = Math.max(1, Math.ceil($scope.pagination.closed.length / $scope.pagination.closed.itemsPerPage));
-	});
+		// prepare the open data
+		var sourceOpen =
+		{
+			datatype: "json",
+			datafields: [
+				{ name: 'title'},
+				{ name: 'creator', map: 'creator>name'},
+				{ name: 'startTime'},
+				{ name: 'progress'},
+				{ name: 'gameId'}
+			],
+			id: 'gameId',
+			localdata: $scope.open_games
+		};
+		var dataAdapterOpen = new $.jqx.dataAdapter(sourceOpen);
+		$scope.openSettings =
+		{
+			width: '100%',
+			pageable: true,
+			autoheight: true,
+			autorowheight: true,
+			source: dataAdapterOpen,
+			columnsresize: true,
+			columns: [
+			    { text: 'Title', width: '40%', datafield: 'title' },
+			    { text: 'Created By', width: '25%', datafield: 'creator' },
+			    { text: 'Creation Date', width: '18%', datafield: 'startTime' },
+			    { text: 'Progress', width: '7%', datafield: 'progress', cellsRenderer: function (row, columnDataField, value) {
+					var r = '<div class="jqx-grid-cell-left-align" style="margin-top: 4px; margin-bottom: 4px;">';
+					r = r.concat((Math.round(value * 100) / 100) + "%");
+					return r.concat("</div>");
+					}
+			    },
+			    { text: 'Detail', width: '10%', datafield: 'gameId', cellsRenderer: function (row, columnDataField, value) {
+					var r = '<div class="jqx-grid-cell-left-align" style="margin-top: 4px; margin-bottom: 4px;">';
+					r = r.concat("<jqx-link-button jqx-width='75' jqx-height='25'><a href='#/game-requirements/game_page?gameId=" + value + "'>Detail</a></jqx-link-button>");
+					return r.concat("</div>");
+					}
+			    }
+			]
+		};
+		$scope.createWidgetOpen = true;
+		
+		// prepare the closed data
+		var sourceClosed =
+		{
+			datatype: "json",
+			datafields: [
+				{ name: 'title'},
+				{ name: 'creator', map: 'creator>name'},
+				{ name: 'startTime'},
+				{ name: 'progress'},
+				{ name: 'gameId'}
+			],
+			id: 'gameId',
+			localdata: $scope.closed_games
+		};
+		var dataAdapterClosed = new $.jqx.dataAdapter(sourceClosed);
+		$scope.closedSettings =
+		{
+			width: '100%',
+			pageable: true,
+			autoheight: true,
+			autorowheight: true,
+			source: dataAdapterClosed,
+			columnsresize: true,
+			columns: [
+			    { text: 'Title', width: '40%', datafield: 'title' },
+			    { text: 'Created By', width: '25%', datafield: 'creator' },
+			    { text: 'Creation Date', width: '18%', datafield: 'startTime' },
+			    { text: 'Progress', width: '7%', datafield: 'progress', cellsRenderer: function (row, columnDataField, value) {
+					var r = '<div class="jqx-grid-cell-left-align" style="margin-top: 4px; margin-bottom: 4px;">';
+					r = r.concat((Math.round(value * 100) / 100) + "%");
+					return r.concat("</div>");
+					}
+			    },
+			    { text: 'Detail', width: '10%', datafield: 'gameId', cellsRenderer: function (row, columnDataField, value) {
+					var r = '<div class="jqx-grid-cell-left-align" style="margin-top: 4px; margin-bottom: 4px;">';
+					r = r.concat("<jqx-link-button jqx-width='75' jqx-height='25'><a href='#/game-requirements/game_page?gameId=" + value + "'>Detail</a></jqx-link-button>");
+					return r.concat("</div>");
+					}
+			    }
+			]
+		};
+		$scope.createWidgetClosed = true;
+		
+		$('#jqxTabs').jqxTabs({ height: 500, width: '100%' });
+	    var index = 0;
+	    $('#jqxTabs').on('tabclick', function (event) {
+	        if (event.args.item == $('#unorderedList').find('li').length - 1) {
+	            //go to create game page
+	        	$location.path('/game-requirements/create_game');
+	        }
+	    });
+	    $('#unorderedList').css('visibility', 'visible');
+	 }).error(function (data, status) {
+		 alert(status);
+	 });
 	
-    
-    $scope.range = function (oc) {
-        var ret = [];
-        var showPages = Math.min(5, oc.totalPages);
-        
-        var start = Math.max(1, oc.currentPage - Math.floor(showPages / 2));
-        var end = Math.min(oc.totalPages, oc.currentPage + Math.ceil(showPages / 2))
-        
-        if(start == 1)
-        {
-        	end = start + showPages - 1;
-        }
-        if(end == oc.totalPages)
-        {
-        	start = end - showPages + 1;
-        }
-        
-        for (var i = start; i < start + showPages; i++) {
-            ret.push(i);
-        }
-        
-        return ret;
-    };
-    
-    $scope.prevPage = function (oc) {
-        if (oc.currentPage > 0) {
-        	oc.currentPage--;
-        }
-    };
-    
-    $scope.nextPage = function (oc) {
-        if (oc.currentPage < oc.totalPages - 1) {
-        	oc.currentPage++;
-        }
-    };
-    
-    $scope.setPage = function (oc) {
-    	oc.currentPage = this.n -1;
-    };
-
     $scope.goGame = function (gameId) {
     	$location.path('/game-requirements/game_page').search('gameId', gameId);
 	};
 	
-
-    $scope.goCreateGame = function (gameId) {
-    	$location.path('/game-requirements/create_game');
-	};
 });
