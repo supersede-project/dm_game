0aa74d10b97e8ebca1e6ba3cfdbbb67642b0dc58
Mauro Fruet <mauro.fruet@deltainformatica.eu>
2017-01-18 11:32:19 +0100

    use gradle wrapper to compile all applications

--- applications/supersede-dm-app/buildall.sh
+++ applications/supersede-dm-app/buildall.sh
@@ -1,42 +1,46 @@
 echo -e "\nCleaning supersede-dm-app...\n"
-gradle clean
+./gradlew clean
 
 cd supersede-dm-core
 echo -e "\nBuilding supersede-dm-core...\n"
-gradle build --refresh-dependencies
+./gradlew build --refresh-dependencies
 
 cd ../supersede-dm-datastore
 echo -e "\nBuilding supersede-dm-datastore...\n"
-gradle build --refresh-dependencies
+./gradlew build --refresh-dependencies
 
 cd ../supersede-dm-integration
 echo -e "\nBuilding supersede-dm-integration...\n"
-gradle build --refresh-dependencies
+./gradlew build --refresh-dependencies
 
 cd ../supersede-dm-executor
 echo -e "\nBuilding supersede-dm-executor...\n"
-gradle build --refresh-dependencies
+./gradlew build --refresh-dependencies
 
 cd ../supersede-dm-ahprp
 echo -e "\nBuilding supersede-dm-ahprp...\n"
-gradle build --refresh-dependencies
+./gradlew build --refresh-dependencies
 
 cd ../supersede-dm-ahprp-ui
 echo -e "\nBuilding supersede-dm-ahprp-ui...\n"
-gradle build --refresh-dependencies
+./gradlew build --refresh-dependencies
 
 cd ../supersede-dm-garp
 echo -e "\nBuilding supersede-dm-garp...\n"
-gradle build --refresh-dependencies -x test
+./gradlew build --refresh-dependencies -x test
+
+cd ../supersede-dm-garp-ui
+echo -e "\nBuilding supersede-dm-garp-ui...\n"
+./gradlew build --refresh-dependencies -x test
 
 cd ../supersede-dm-planning-jmetal
 echo -e "\nBuilding supersede-dm-planning-jmetal...\n"
-gradle build --refresh-dependencies
+./gradlew build --refresh-dependencies
 
 cd ../supersede-dm-orchestrator
 echo -e "\nBuilding supersede-dm-orchestrator...\n"
-gradle build --refresh-dependencies
+./gradlew build --refresh-dependencies
 
 cd ../
 echo -e "\nBuilding supersede-dm-app...\n"
-gradle build --refresh-dependencies -x test
+./gradlew build --refresh-dependencies -x test
