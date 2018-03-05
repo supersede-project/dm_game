56227fd20182e997df5fe8c2c6a40d6f530b4d27
Andrea Gottardi <andreagot@users.noreply.github.com>
2018-03-05 10:50:12 +0100

    test on button graphics

--- applications/supersede-dm-app/src/main/resources/static/js/process.js
+++ applications/supersede-dm-app/src/main/resources/static/js/process.js
@@ -357,7 +357,7 @@ app.controllerProvider
 																// the status
 																// bar.
 																var container = $("<div style='overflow: hidden; position: relative; margin: 5px;'></div>");
-																var GAButton = $("<button>New GA Game</button>");
+																var GAButton = $("<button>New GA Game</button> </br>");
 																var AHPButton = $("<button>New AHP Game</button>");
 																container
 																		.append(GAButton);
